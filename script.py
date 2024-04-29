9b534a632d5912b6b28ff14c899784f34a5f6aea
Saille <167231241+xSaille@users.noreply.github.com>
2024-04-29 15:21:06 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,56 @@
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+stacks = []
+left_stack = Stack("Left")
+right_stack = Stack("Right")
+middle_stack = Stack("Middle")
+stacks.append(left_stack)
+stacks.append(right_stack)
+stacks.append(middle_stack)
+
+num_disks = int(input("\nHow many disks do you want to play with?\n"))
+while (num_disks < 3):
+    num_disks = int(input("Enter a number greater than or equal to 3\n"))
+for i in range(num_disks, 0, -1):
+    left_stack.push(i)
+num_optimal_moves = 2 ** num_disks - 1
+print(f"\nThe fastest you can solve this game is in {num_optimal_moves} moves")
+
+
+def get_input():
+    choices = [stack.get_name()[0] for stack in stacks]
+    while True:
+        for i in range(len(stacks)):
+            name = stacks[i].get_name()
+            letter = choices[i]
+            print(f"Enter {letter} for {name}")
+        user_input = input("")
+        if user_input in choices:
+            for i in range(len(stacks)):
+                if user_input == choices[i]:
+                    return stacks[i]
+
+
+num_user_moves = 0
+while (right_stack.get_size() != num_disks):
+    print("\n\n\n...Current Stacks...")
+    for stack in stacks:
+        stack.print_items()
+        while True:
+            print("\nWhich stack do you want to move from?\n")
+            from_stack = get_input()
+            print("\nWhich stack do you want to move to?\n")
+            to_stack = get_input()
+
+            if from_stack.is_empty():
+                print("\n\nInvalid Move. Try Again")
+            elif to_stack.is_empty() or from_stack.peek() < to_stack.peek():
+                disk = from_stack.pop()
+                to_stack.push(disk)
+                num_user_moves += 1
+                break
+            else:
+                print("\n\nInvalid Move. Try Again")
+print(f"\n\nYou completed the game in {num_user_moves} moves, and the optimal number of moves is {num_optimal_moves}")
